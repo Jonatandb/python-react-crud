725f6b869e8cbcb597cedb7d841525fcdf6cfb9f
Jonatandb <jonatandb@gmail.com>
2022-05-08 17:42:54 -0300

    About page added

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,7 +1,40 @@
 import React from 'react'
 
 const About = () => {
-  return <h1>About</h1>
+  return (
+    <>
+      <h1>About</h1>
+      <p>This is a simple React app that uses the following technologies:</p>
+      <ul>
+        <li>
+          <a href='https://es.reactjs.org/' alt='React'>
+            React
+          </a>
+        </li>
+        <li>
+          <a href='https://reactrouter.com/' alt='React Router'>
+            React Router
+          </a>
+        </li>
+        <li>
+          <a href='https://flask.palletsprojects.com/en/2.1.x/' alt='Flask'>
+            Flask
+          </a>
+        </li>
+
+        <li>
+          <a href='https://www.mongodb.com/es' alt='MongoDB'>
+            MongoDB
+          </a>
+        </li>
+        <li>
+          <a href='https://bootswatch.com/' alt='Bootswatch'>
+            Bootswatch
+          </a>
+        </li>
+      </ul>
+    </>
+  )
 }
 
 export default About
