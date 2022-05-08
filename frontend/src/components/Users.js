1d6ddc4e416b6f69db38e67d33267d9faada330b
Jonatandb <jonatandb@gmail.com>
2022-05-08 18:44:53 -0300

    Adding create user form

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Users = () => {
-  return <h1>Users</h1>
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSumbit = e => {
+    e.preventDefault()
+    console.log({ name, email, password })
+  }
+
+  return (
+    <div className='row'>
+      <div className='col-md-4'>
+        <form onSubmit={handleSumbit} className='card card-body'>
+          <div className='form-group'>
+            <label htmlFor='name'>Name</label>
+            <input
+              type='text'
+              name='name'
+              className='form-control mb-2'
+              placeholder='Enter name'
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              name='email'
+              className='form-control mb-2'
+              placeholder='Enter email'
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              className='form-control mb-2'
+              placeholder='Enter password'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+          <button className='btn btn-primary btn-block'>Create</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Users
