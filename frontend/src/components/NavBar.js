d9c8704f4977f3e56fed85baab3931fcbe5a3c94
Jonatandb <jonatandb@gmail.com>
2022-05-08 18:46:00 -0300

    Adding of some margin to Navbar main title

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
   return (
     <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-      <Link className='navbar-brand' to='/'>
+      <Link className='navbar-brand mx-2' to='/'>
         Flask & React CRUD
       </Link>
       <button
